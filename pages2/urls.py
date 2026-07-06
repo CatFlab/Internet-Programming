95e78dbd801ca3f6108e3ebdc96f0ed864d78637
CatFlab <YMAH473593@PAAET.EDU.KW>
2026-07-06 11:52:14 +0300

    Added 3 new files "Static, css, images" added new links for urls and added new direction in settings

--- pages2/urls.py
+++ pages2/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    #path('services/', views.services, name='services'),
-    path('contact/', views.contact, name='contact'),
+    path('', views.index, name='home'),
+    path('index.html', views.index, name='index'),
+    path('About_us.html', views.about, name='about'),
+    path('Contact_us.html', views.contact, name='contact'),
+    path('Event.html', views.event, name='event'),
+    path('Register.html', views.register, name='register'),
+    path('Login.html', views.login, name='login'),
+    path('Activity_Center.html', views.activitycenter, name='activitycenter'),
+    path('Interactive_map.html', views.interactivemap, name='interactivemap'),
 ]
--- pages2/views.py
+++ pages2/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'About_us.html')
+
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'Contact_us.html')
+
+def event(request):
+    return render(request, 'Event.html')
+
+def register(request):
+    return render(request, 'Register.html')
+
+def login(request):
+    return render(request, 'Login.html')
+
+def activitycenter(request):
+    return render(request, 'Activity_Center.html')
+
+def interactivemap (request):
+    return render(request, 'Interactive_Map.html')

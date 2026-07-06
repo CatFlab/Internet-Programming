ad1d8ac99cbeb53150d19d200d27dbccfebc137d
CatFlab <YMAH473593@PAAET.EDU.KW>
2026-07-06 15:53:53 +0300

    DTL and inheritance

--- pages2/views.py
+++ pages2/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {"active_page": "home"})
 
 def about(request):
-    return render(request, 'About_us.html')
+    return render(request, 'About_us.html', {"active_page": "about"})
 
 def contact(request):
-    return render(request, 'Contact_us.html')
+    return render(request, 'Contact_us.html', {"active_page": "contact"})
 
 def event(request):
-    return render(request, 'Event.html')
+    return render(request, 'Event.html', {"active_page": "event"})
 
 def register(request):
-    return render(request, 'Register.html')
+    return render(request, 'Register.html', {"active_page": "register"})
 
 def login(request):
-    return render(request, 'Login.html')
+    return render(request, 'Login.html', {"active_page": "login"})
 
 def activitycenter(request):
-    return render(request, 'Activity_Center.html')
+    return render(request, 'Activity_Center.html', {"active_page": "activitycenter"})
 
 def interactivemap (request):
-    return render(request, 'Interactive_Map.html')
+    return render(request, 'Interactive_Map.html', {"active_page": "map"})

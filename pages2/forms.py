2408d2eb8bbf08f3d7fbb46677400a876e7909c4
CatFlab <YMAH473593@PAAET.EDU.KW>
2026-07-13 17:10:58 +0300

    changes to forms to make it easier for users to enter date an time

--- pages2/forms.py
+++ pages2/forms.py
@@ -5,3 +5,11 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['name', 'organizer', 'description', 'date', 'time', 'room']
+        widgets = {
+            "date": forms.DateInput(attrs={
+                "placeholder": "YYYY-MM-DD",
+            }),
+            "time": forms.TimeInput(attrs={
+                "placeholder": "HH:MM:SS",
+            }),
+        }

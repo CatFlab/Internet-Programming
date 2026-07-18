a7fc30ce2061a646dec2ef6db9f6223c0aea66fb
CatFlab <YMAH473593@PAAET.EDU.KW>
2026-07-18 16:45:08 +0300

    New User and User based events

--- pages2/models.py
+++ pages2/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Map(models.Model):
     name = models.CharField(max_length=100)
@@ -43,6 +43,7 @@ class Event(models.Model):
     date = models.DateField()
     time = models.TimeField()
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='events')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userevents', null=True, blank=True)
 
     def __str__(self):
         return self.name
--- pages2/views.py
+++ pages2/views.py
@@ -1,10 +1,11 @@
-from urllib import request
-
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth import login
 from .models import Map, Event
-from .forms import EventForm
+from .forms import EventForm 
+from django.contrib.auth.models import User
+
 
 
 def index(request):
@@ -42,18 +43,24 @@ def register(request):
 
 def event(request):
     event_form = EventForm()
-    events = Event.objects.all()
+    if request.user.is_authenticated:
+        events = Event.objects.filter(user=request.user)
+    else:
+        events = Event.objects.filter(user__is_superuser=True)
     return render(request, 'Event/Event.html', {"active_page": "event", "events": events, "event_form": event_form})
 
 def event_detail(request, id):
     event = get_object_or_404(Event, id=id)
     return render(request, 'Event/Event_Detail.html', {"active_page": "event", "event": event})
 
+@login_required
 def event_create(request):
     if request.method == "POST":
         event_form = EventForm(request.POST)
         if event_form.is_valid():
-            event_form.save()
+            event = event_form.save(commit=False)
+            event.user = request.user
+            event.save()
             action = request.POST.get('action')
             if action == 'save_add':
                 return redirect('event_create')
@@ -64,21 +71,21 @@ def event_create(request):
 
     return redirect('event')
 
+@login_required
 def event_edit(request, id):
-    event = get_object_or_404(Event, id=id)
+    event = get_object_or_404(Event, id=id, user=request.user)
     if request.method == "POST":
         event_form = EventForm(request.POST, instance=event)
         if event_form.is_valid():
             event_form.save()
             return redirect('event_detail', id=event.id)
 
-    else:
-        event_form = EventForm(instance=event)
-
+    event_form = EventForm(instance=event)
     return render(request, 'Event/Event_Edit.html', {"active_page": "event", "event": event, "event_form": event_form})
 
+@login_required
 def event_delete(request, id):
-    event = get_object_or_404(Event, id=id)
+    event = get_object_or_404(Event, id=id, user=request.user)
     if request.method == "POST":
         event.delete()
         return redirect('event')
